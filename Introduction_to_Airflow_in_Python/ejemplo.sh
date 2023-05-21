d5ba8131594e7622d2885b1f8ce44beb87a8206b
alexandergribenchenko <alexandergribenchenko@gmail.com>
2023-05-21 09:13:45 -0500

    commit home

---
+++ Introduction_to_Airflow_in_Python/ejemplo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Crear la carpeta en la misma ubicación donde se ejecuta el script
+mkdir "./carpeta_01"
+cd "./carpeta_01"
+
+wget https://data.sba.gov/dataset/8aa276e2-6cab-4f86-aca4-a7dde42adf24/resource/aab8e9f9-36d1-42e1-b3ba-e59c79f1d7f0/download/ppp-data-dictionary.xlsx
+wget https://data.sba.gov/dataset/8aa276e2-6cab-4f86-aca4-a7dde42adf24/resource/137436c9-408e-47e9-a7f3-b9a1871c4e11/download/public_up_to_150k_1_230331.csv
+wget https://data.sba.gov/dataset/8aa276e2-6cab-4f86-aca4-a7dde42adf24/resource/67b6b208-7116-4e8d-9a56-7168b42cda4a/download/public_up_to_150k_2_230331.csv
--- Introduction_to_Airflow_in_Python/ejemplo_archivo_sh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script para crear una carpeta en la misma ubicación donde se ejecuta
-
-# Crear la carpeta en la misma ubicación donde se ejecuta el script
-mkdir "./carpeta_nueva"
